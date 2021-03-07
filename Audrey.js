ca0874ef911a9c27bcd127656b23f0842e5aa74a
student <audreymunyiva@gmail.com>
2021-03-07 09:59:49 +0300

    first commit

---
+++ Audrey.js
@@ -0,0 +1,7 @@
+function fruits(fruits_list){
+    fruits_list=["strawberries","mangoes","kiwi","grapes","guava"];
+    fruits_list.forEach(element=>console.log(element));
+    console.log(fruits_list[0]);
+
+}
+fruits();
